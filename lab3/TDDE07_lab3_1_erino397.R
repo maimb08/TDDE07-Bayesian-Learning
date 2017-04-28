bd71d61e13f794828225d72c98543617bf747c31
eriklindernoren <eriklindernoren@live.se>
2017-04-28 17:21:48 +0200

    Lab 3: A1.1

--- lab3/TDDE07_lab3_1_erino397.R
+++ lab3/TDDE07_lab3_1_erino397.R
@@ -1,29 +1,27 @@
 require(MASS)
 require(geoR)
 
-
 grid_w = 5
 grid_h = 4
-
 
 # Lab 3 - Assignment 1
 
 data = read.table("data/rainfall.txt", header=FALSE)[,1]
 
-n = lenght(data)
+n = length(data)
 
 # (a)
 
-# Initial variance prior parameters
+data_mean = mean(data)
+
+# Prior parameters for sigma (variance)
 v0 = 1
 sigma0 = 1 / v0
 
-data_mean = mean(data)
-
-# Starting value for sigma
-sigma = rinvchisq(n=1, n, 1)
-
 n_draws = 4000
+
+# Initial value for sigma
+sigma = rinvchisq(n=1, v0, sigma0)
 
 gibbs_draws = matrix(0,n_draws,2)
 for(i in 1:n_draws){
@@ -32,8 +30,10 @@ for(i in 1:n_draws){
   gibbs_draws[i,] = c(mu, sigma)
 }
 
+
 mean_draws = gibbs_draws[,1]
 var_draws = gibbs_draws[,2]
+
 
 # Calculate mean of batches of 5 draws to visualize the
 # auto correlation between sequential draws
@@ -49,8 +49,8 @@ for (i in 1:n_draws){
 pdf("plots/3_1_gibbs_conv_mu.pdf", width=grid_w, height=grid_h)
 
 # Plot the auto correlation (convergence) between draws of mu
-min_mean = min(mean_draws)
-max_mean = max(mean_draws)
+min_mean = min(mean_means)
+max_mean = max(mean_means)
 plot(mean_means, 
      type="l", 
      ylim=c(min_mean, max_mean), 
