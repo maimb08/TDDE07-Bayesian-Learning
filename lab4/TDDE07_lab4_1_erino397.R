8f806d6044e8f62774dafa7ee367a734d14d4a0e
eriklindernoren <eriklindernoren@live.se>
2017-05-13 12:32:05 +0200

    Lab 4: (a)

--- lab4/TDDE07_lab4_1_erino397.R
+++ lab4/TDDE07_lab4_1_erino397.R
@@ -2,7 +2,7 @@ require(MASS)
 require(geoR)
 
 grid_w = 5
-grid_h = 4
+grid_h = 5
 
 # -------
 #  Lab 4
@@ -14,10 +14,10 @@ data = read.table("data/eBayNumberOfBidderData.dat", header=TRUE)
 n = length(data)
 n_features = ncol(data) - 2 # Except y and const
 
-feature_labels = colnames(data[,2:n_features])
+feature_labels = colnames(data[,2:ncol(data)])
   
 y = data$nBids
-X = as.matrix(data[,3:ncol(data)])
+X = as.matrix(data[,2:ncol(data)])
 
 X_X = t(X)%*%X
 
@@ -27,6 +27,25 @@ X_X = t(X)%*%X
 
 glm_model = glm(nBids ~ 0 + ., data = data, family = poisson)
 
+pdf("./plots/4_1_1_mle_beta.pdf", height=grid_h)
+
+par(mfrow=c(2,4), oma = c(0, 0, 3, 0))
+for (i in 2:ncol(X)){
+  mean = glm_model$coefficients[i]
+  std_dev = summary(glm_model)[["coefficients"]][,2][i]
+  x_grid = seq(mean-4*std_dev, mean+4*std_dev, 0.001)
+  plot(x_grid,
+       dnorm(x_grid, mean=mean, sd=std_dev),
+       type="l",
+       lwd=2,
+       ylab="Density",
+       xlab=expression(beta),
+       main=feature_labels[i])
+}
+title("Normal approximation of beta", outer=TRUE, cex=1.5)
+
+dev.off()
+
 # -----
 #  (b)
 # -----
@@ -34,13 +53,6 @@ glm_model = glm(nBids ~ 0 + ., data = data, family = poisson)
 # Beta prior parameters (Zellner’s g-prior)
 mu0 = 0
 covar0 = 100 * ginv(X_X)
-
-# This is the log posterior density of the beta(s+a,f+b) density
-LogPostPoisson = function(theta, s, f, a, b){
-  log_prior = log(dnorm(mu0, covar0))
-  log_likelihood = 
-  return(logPost)
-}
 
 # This is the log posterior density of the beta(s+a,f+b) density
 # LogPostBernBeta <- function(theta, s, f, a, b){
